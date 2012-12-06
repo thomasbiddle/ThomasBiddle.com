21970604c40fc0a5ee755727a50da312f9b2419d
TJ Biddle <me@ThomasBiddle.com>
2012-12-05 20:00:54 -0800

    Fixed typo on conent for vhost, and commented out others while testing

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -10,7 +10,46 @@ class thomasbiddle_com {
   file { '/etc/apache2/sites-available/thomasbiddle.com':
     ensure => present,
     require => Package['apache2'],
-    content => template(thomasbiddle_com/apache2.erb"),
+    content => template('thomasbiddle_com/apache2.erb'),
   }
+
+  # Ensure the file structure is in place.
+  file { '/srv/':
+    ensure => directory,
+  }
+  file { '/srv/www/':
+    ensure => directory,
+    require => File['/srv/'],
+  }
+  file { '/srv/www/thomasbiddle.com':
+    ensure => directory,
+    require => File['/srv/www/'],
+  }
+
+# The below configuration will be applied later as it has not been tested.
+#  # Install the Apache package.
+#  class {'apache':  }
+#
+#  apache::vhost { 'www.thomasbiddle.com':
+#    ensure          => 'present',
+#    vhost_name      => 'thomasbiddle.com',
+#    port            => '80',
+#    docroot         => '/srv/www/thomasbiddle.com',
+#    logroot         => '/var/log/apache2/',
+#    serveradmin     => 'biddle.thomas@gmail.com',
+#    serveraliases   => ['thomasbiddle.com', 'thomasbiddle.co',],
+#  }
+#  cron::job{
+#    'mysqlbackup'
+#      minute  => '1',
+#      hour    => '*',
+#      date    => '*',
+#      month   => '*',
+#      weekday => '*',
+#      user    => 'root',
+#      command => 'mysqlbackup mydb';
+#  }
+
+
 
 }
