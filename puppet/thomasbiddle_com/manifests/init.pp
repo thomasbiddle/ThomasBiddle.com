0b75303623d180ab6d7309f24dfc473d45897cba
TJ Biddle <me@ThomasBiddle.com>
2012-12-06 22:17:13 -0500

    Removing Package[apache2] as it will cause duplicate declartions.

--- puppet/thomasbiddle_com/manifests/init.pp
+++ puppet/thomasbiddle_com/manifests/init.pp
@@ -1,14 +1,7 @@
-
 # Setup ThomasBiddle.com
 class thomasbiddle_com {
 
-  # Ensure apache2 is installed and up to date.
-  package { 'apache2':
-    ensure => 'latest',
-  }
-
   # Setup the Apache Virtual Host
-  # (After testing, will set this up using the PuppetLabs apache module)
   file { '/etc/apache2/sites-available/thomasbiddle.com':
     ensure  => present,
     require => Package['apache2'],
